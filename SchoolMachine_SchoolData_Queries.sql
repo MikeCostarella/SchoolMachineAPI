aed1cbef94cbb4cdc9ccebe20f89a20fd2275618
Mike Costarella <mcosti@sprynet.com>
2019-03-05 22:42:11 -0500

    Created a query to list each school that has students, from highest to lowest, in terms of number of students. Then alphabetically by name for schools with the same number of students.

--- SchoolMachine_SchoolData_Queries.sql
+++ SchoolMachine_SchoolData_Queries.sql
@@ -9,3 +9,15 @@ FROM
 	INNER JOIN schooldata.district
 		ON district_school.district_id = district.id)
 ORDER BY district.name;
+
+
+-- List each school that has students, from highest to lowest, in terms of number of students.
+-- Then alphabetically by name for schools with the same number of students
+-- NOTE: to get all schools change the INNER JOIN to a LEFT JOIN
+SELECT DISTINCT
+	MAX(schooldata.school.name) AS SchoolName,
+	COUNT(school_student.student_id) AS NumberOfStudents
+FROM schooldata.school
+    INNER JOIN schooldata.school_student ON school_student.school_id = school.id
+GROUP BY school.id
+ORDER BY COUNT(school_student.student_id) DESC, SchoolName;
