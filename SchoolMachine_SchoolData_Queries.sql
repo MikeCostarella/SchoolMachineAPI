cdd1b34c33802dd16dab91855bc93b191c8b1629
Mike Costarella <mcosti@sprynet.com>
2019-03-06 20:44:00 -0500

    Added an SQL query to List All Districts, Schools, Grade Levels and Students in that order

--- SchoolMachine_SchoolData_Queries.sql
+++ SchoolMachine_SchoolData_Queries.sql
@@ -3,13 +3,26 @@ SELECT
   district.name AS "DistrictName",
   school.name AS "SchoolName"
 FROM 
-  ((schooldata.school  
-	INNER JOIN schooldata.district_school
-		ON district_school.school_id = school.id)		
-	INNER JOIN schooldata.district
-		ON district_school.district_id = district.id)
+  schooldata.school
+	INNER JOIN schooldata.district_school ON district_school.school_id = school.id
+	INNER JOIN schooldata.district ON district_school.district_id = district.id
 ORDER BY district.name;
 
+-- List All Districts, Schools, Grade Levels and Students in that order
+SELECT
+	districts.name AS "District",
+	schools.name AS "School",
+	schoolstudents.grade_level AS "GradeLevel",
+	students.first_name AS "FirstName",
+	students.middle_name AS "MiddleName",
+	students.last_name AS "LastName"
+FROM
+  schooldata.student AS students
+	RIGHT JOIN schooldata.school_student AS schoolstudents ON schoolstudents.student_id = students.id
+	RIGHT JOIN schooldata.school AS schools ON schoolstudents.school_id = schools.id
+	RIGHT JOIN schooldata.district_school AS districtschools ON districtschools.school_id = schools.id
+	RIGHT JOIN schooldata.district AS districts ON districtschools.district_id = districts.id
+ORDER BY "District", "School", "GradeLevel", "LastName", "FirstName", "MiddleName";
 
 -- List each school that has students, from highest to lowest, in terms of number of students.
 -- Then alphabetically by name for schools with the same number of students
