d7a1e572872d2399173afb01786eafa52ad53559
Mike Costarella <mcosti@sprynet.com>
2019-03-11 22:25:23 -0400

    Reformatting of IUserService

--- SchoolMachine.API/Services/IUserService.cs
+++ SchoolMachine.API/Services/IUserService.cs
@@ -7,10 +7,15 @@ namespace SchoolMachine.API.Services
     public interface IUserService
     {
         User Authenticate(string username, string password);
+
         IEnumerable<User> GetAll();
+
         User GetById(Guid id);
+
         User Create(User user, string password);
+
         void Update(User user, string password = null);
+
         void Delete(Guid id);
     }
 }
