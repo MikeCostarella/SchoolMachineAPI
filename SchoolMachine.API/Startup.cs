0b2cdd6d3b9d08401ed287d91f0bbdfc2da3abc1
Mike Costarella <mcosti@sprynet.com>
2020-04-12 21:35:12 -0400

    Added ApplicationInsights

--- SchoolMachine.API/Startup.cs
+++ SchoolMachine.API/Startup.cs
@@ -36,6 +36,7 @@ namespace SchoolMachine.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApplicationInsightsTelemetry();
             services.ConfigureCors();
             services.ConfigureIISIntegration();
             services.ConfigureLoggerService();
