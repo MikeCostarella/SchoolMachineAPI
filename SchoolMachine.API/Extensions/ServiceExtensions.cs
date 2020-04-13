80f27fb2dc049db6077b889b4eeedd357acd75a0
Mike Costarella <mcosti@sprynet.com>
2020-04-13 16:01:42 -0400

    Added code for swagger

--- SchoolMachine.API/Extensions/ServiceExtensions.cs
+++ SchoolMachine.API/Extensions/ServiceExtensions.cs
@@ -151,8 +151,33 @@ namespace SchoolMachine.API.Extensions
                 var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
                 c.IncludeXmlComments(xmlPath);
-            });
+                //c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                //{
+                //    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer { token }\"",
+                //    Name = "Authorization",
+                //    In = ParameterLocation.Header,
+                //    Type = SecuritySchemeType.ApiKey,
+                //    Scheme = "Bearer"
+                //});
+                //c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                //{
+                //    {
+                //        new OpenApiSecurityScheme
+                //        {
+                //            Reference = new OpenApiReference
+                //            {
+                //                Type = ReferenceType.SecurityScheme,
+                //                Id = "Bearer"
+                //            },
+                //            Scheme = "Bearer",
+                //            Name = "Bearer",
+                //            In = ParameterLocation.Header,
 
+                //        },
+                //        new List<string>()
+                //    }
+                //});
+            });
         }
 
         public static void ConfigureUserService(this IServiceCollection services, IConfiguration configuration)
